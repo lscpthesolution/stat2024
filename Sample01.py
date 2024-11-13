747af6f6fe1989741bbcc48cf03f7689845feb03
Minhyuk Adam Oh <adam.oh@libertas-scholars.orgclera>
2024-11-13 13:11:39 +0900

    updated

---
+++ Sample01.py
@@ -0,0 +1,20 @@
+sample01 = [4,77,8,2,9,11,66]
+sample02 = [66,55,4,3,2,5,21,85,22]
+sample03 = [5,6,7,8,33,33,33,33,22,22,11,11,11,11,1,2,3,66,77,23]
+
+
+# return mean
+def findMean(givenList : list) -> int:
+    pass
+
+# return mode 
+def findMode(givenList : list) -> int:
+    pass
+
+# return median
+def findMedian(givenList : list) -> int:
+    pass
+
+# return range
+def findRange(givenList : list) -> int:
+    pass
